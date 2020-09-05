7b64a6fbef94731b250df3c8e1aba814c380ec51
SuyamaDaichi <symdit@gmail.com>
2020-09-06 00:49:08 +0900

    APIGWのLambdaプロキシに対応

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
+import { Response } from './APIGWResponse.d';
 import { Context, APIGatewayEvent } from "aws-lambda";
 import axios from 'axios';
 import * as dotenv from "dotenv";
 dotenv.config();
 
-export async function handler(event: APIGatewayEvent, context?: Context): Promise<string> {
+export async function handler(event: APIGatewayEvent, context?: Context): Promise<Response> {
     const token = process.env['SECRET'];
     const baseRequest = axios.create({
         baseURL: `url`,
@@ -14,6 +15,22 @@ export async function handler(event: APIGatewayEvent, context?: Context): Promis
     const res = await baseRequest.get('path')
         .catch(e => {
             console.error(e);
+            return {
+                isBase64Encoded: false,
+                statusCode: 500,
+                headers: {},
+                body: JSON.stringify({
+
+                })
+            };
         });
-    return res ? 'success!' : 'failured';
+
+    return {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: {},
+        body: JSON.stringify({
+            propName: 'success'
+        })
+    };
 }
